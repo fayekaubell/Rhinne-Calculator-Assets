48ad57e36391e1f5a53e0ff2a0b5646c7f9d1aea
fayekaubell <103764732+fayekaubell@users.noreply.github.com>
2025-07-01 16:42:44 -0400

    Create patterns-data.js
    Add patterns database

---
+++ patterns-data.js
@@ -0,0 +1,305 @@
+// Wallpaper patterns data converted from CSV
+// This file contains all the pattern information needed for the calculator
+
+const PATTERNS_DATA = [
+  {
+    "pattern_name": "Megaflora: Dusk",
+    "sku": "W-MEG-DUS-2",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-DUS-2-repeat.jpg"
+  },
+  {
+    "pattern_name": "Megaflora: Tomato",
+    "sku": "W-MEG-DUS",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-DUS-repeat.jpg"
+  },
+  {
+    "pattern_name": "Megaflora: Blue",
+    "sku": "W-MEG-BLU",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-BLU-repeat.jpg"
+  },
+  {
+    "pattern_name": "Megaflora: Orange",
+    "sku": "W-MEG-ORA",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-ORA-repeat.jpg"
+  },
+  {
+    "pattern_name": "Megaflora: Green",
+    "sku": "W-MEG-GRE",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-GRE-repeat.jpg"
+  },
+  {
+    "pattern_name": "Megaflora: Pink",
+    "sku": "W-MEG-PIN",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-PIN-repeat.jpg"
+  },
+  {
+    "pattern_name": "Megaflora: Purple",
+    "sku": "W-MEG-PUR",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-PUR-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Moss",
+    "sku": "W-WON-MOS",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-MOS-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Coral",
+    "sku": "W-WON-COR",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-COR-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Navy",
+    "sku": "W-WON-NAV",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-NAV-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Vanilla",
+    "sku": "W-WON-VAN",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-VAN-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Royal",
+    "sku": "W-WON-ROY",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-ROY-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Steel",
+    "sku": "W-WIL-STE",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-STE-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Mint",
+    "sku": "W-WIL-MIN",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-MIN-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Orange",
+    "sku": "W-WIL-ORA",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-ORA-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Green",
+    "sku": "W-WIL-GRE",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-GRE-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Fuchsia",
+    "sku": "W-WIL-FUC",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-FUC-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Blue",
+    "sku": "W-WIL-BLU",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-BLU-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Purple",
+    "sku": "W-WIL-PUR",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-PUR-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Pink",
+    "sku": "W-WIL-PIN",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-PIN-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Black",
+    "sku": "W-WON-BLA",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-BLA-repeat.jpg"
+  },
+  {
+    "pattern_name": "Megaflora: Rust",
+    "sku": "W-MEG-RUS",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-MEG-RUS-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Gold",
+    "sku": "W-WON-GOL",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-GOL-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Pink",
+    "sku": "W-WON-PIN",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-PIN-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Red",
+    "sku": "W-WON-RED",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-RED-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wonderland: Blue",
+    "sku": "W-WON-BLU",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 27,
+    "material_width_inches": 27,
+    "pattern_match": "straight match",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WON-BLU-repeat.jpg"
+  },
+  {
+    "pattern_name": "Wildflower: Yellow",
+    "sku": "W-WIL-YEL",
+    "sale_type": "yard",
+    "repeat_width_inches": 27,
+    "repeat_height_inches": 33.5,
+    "material_width_inches": 27,
+    "pattern_match": "half drop",
+    "min_yard_order": 5,
+    "repeat_url": "https://raw.githubusercontent.com/fayekaubell/Rhinne-Calculator-Assets/9cc0d1998daf307442a54c6f1199f04f15d167d0/patterns/W-WIL-YEL-repeat.jpg"
+  }
+];
+
+// Make the data available globally
+window.PATTERNS_DATA = PATTERNS_DATA;
